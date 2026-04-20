159c25f297b4b8ae0fad4fd34d4f0373dfefbb53
jlangz <jakob.langseth@gmail.com>
2026-04-20 00:23:11 -0700

    Aaagagah just find the right api url env

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  envDir: process.env.VITE_API_URL ? undefined : '..',
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
